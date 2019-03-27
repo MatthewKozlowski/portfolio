d43e057a067b1c560f5be56c00bc0838542221ad
Matthew Kozlowski <maekozlowski@gmail.com>
2019-03-26 19:12:28 -0600

    CSS Styling Update

---
+++ index.js
@@ -0,0 +1,43 @@
+function goToBio() {
+    $('#menu').on('click', '#bio', function(){
+        $('#menu').remove();
+        $('main').append(`
+                <div class="menuContainers">
+                    <h2>Bio:</h2>
+                    <div id="bioListConatiner_1">    
+                        <ul class="bioLists">Knows:
+                            <li class="bioListItems">HTML</li>
+                            <li class="bioListItems">CSS</li>
+                            <li class="bioListItems">Javascript</li>
+                            <li class="bioListItems">jQuery</li>
+                        </ul>
+                    </div>
+                    <div id="bioListConatiner_2">    
+                        <ul class="bioLists">Enjoys:
+                            <li class="bioListItems">Reading (books)</li>
+                            <li class="bioListItems">Video Games (Grand Strategy)</li>
+                            <li class="bioListItems">Outdoor Activities (Camping & Adrenaline Pumping</li>
+                            <li class="bioListItems">Dungeons & Dragons (DMing Homebrews</li>
+                        </ul>
+                    </div>    
+                </div>`);
+    })
+}
+
+function goToProjects() {
+    $('#menu').on('click', '#projects', function(){
+        $('#menu').remove();
+        $('main').append('<h2>IT WORK YO</h2>');
+    })
+}
+
+function goToResume() {
+    $('#menu').on('click', '#resume', function(){
+        $('#menu').remove();
+        $('main').append('<h2>IT WORK YO</h2>');
+    })
+}
+
+goToBio();
+goToProjects();
+goToResume();
